4fdb45f2c38c255bbfb70bd7b6292e151202b2bc
Quentin Bazin <quent42340@gmail.com>
2024-10-26 20:18:10 +0200

    Now smoothing camera position, better tracking but offset is broken.

--- prestart.js
+++ prestart.js
@@ -6,25 +6,24 @@ ig.module('cc-staircase-effect-fix')
 		ig.Camera.EntityTarget.inject(
 		{
 			_lastEntityPos: Vec2.create(),
-			_offset: Vec2.create(),
+			_cameraSmoothingFactor: 0.15,
 
 			init(a, b)
 			{
 				this.parent(a, b);
 
 				Vec2.assign(this._lastEntityPos, this.entity.coll.pos);
-				this._offset.x = 0;
-				this._offset.y = 0;
 			},
 
 			getPos(a)
 			{
+				let olda = Vec2.create();
+				Vec2.assign(olda, a);
 				this.parent(a)
+				Vec2.assign(a, olda);
 	
 				var d = Vec2.create();
 				Vec2.assign(d, this.entity.coll.pos);
-				d.x = d.x - this._offset.x;
-				d.y = d.y - this._offset.y;
 				var ex = d.x - this._lastEntityPos.x;
 				var ey = d.y - this._lastEntityPos.y;
 				var x = d.x;
@@ -42,11 +41,22 @@ ig.module('cc-staircase-effect-fix')
 				this._lastEntityPos.x = x;
 				this._lastEntityPos.y = y;
 
-				this._offset.x += d.x - x;
-				this._offset.y += d.y - y; 
+				// a.x = x + this.entity.coll.size.x / 2;
+				// a.y = y + this.entity.coll.size.y / 2 - Constants.BALL_HEIGHT;
 
-				a.x = x + this.entity.coll.size.x / 2;
-				a.y = y + this.entity.coll.size.y / 2 - Constants.BALL_HEIGHT
+				x += this.entity.coll.size.x / 2;
+				y += this.entity.coll.size.y / 2 - Constants.BALL_HEIGHT;
+
+				let dx = x - a.x;
+				let dy = y - a.y;
+
+				let smoothingFactor = this._cameraSmoothingFactor;
+
+				if (Math.sqrt(dx * dx + dy * dy) > 50)
+					smoothingFactor = 1.0;
+
+				a.x += dx * smoothingFactor;
+				a.y += dy * smoothingFactor;
 			}
 		});
 	});
